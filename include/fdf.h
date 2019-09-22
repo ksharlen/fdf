e2c03100ecdc8b1e67c15b1a06e7c50214810374
Kharazim Sharlene <ksharlen@mi-u3.21-school.ru>
2019-09-22 14:00:35 +0300

    added keysymdef.h

--- include/fdf.h
+++ include/fdf.h
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/18 08:17:24 by ksharlen          #+#    #+#             */
-/*   Updated: 2019/09/20 19:04:33 by ksharlen         ###   ########.fr       */
+/*   Updated: 2019/09/22 13:59:59 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@
 
 # include "libft.h"
 # include "mlx.h"
+# include "keysymdef.h"
 
 #endif
