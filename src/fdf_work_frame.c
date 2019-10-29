9d6f9640b55db2f69e64a0b56fb018ac54461800
Kharazim Sharlene <ksharlen@mi-v3.21-school.ru>
2019-10-29 21:51:52 +0300

    refact fdf_work_window.c

--- src/fdf_work_frame.c
+++ src/fdf_work_frame.c
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/23 22:44:28 by ksharlen          #+#    #+#             */
-/*   Updated: 2019/10/29 21:40:29 by ksharlen         ###   ########.fr       */
+/*   Updated: 2019/10/29 21:51:14 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,49 +26,61 @@
 #define SHIFT_X shift->shift_x
 #define SHIFT_Y shift->shift_y
 
-static t_coord	fdf_rot(t_pixel *elem, struct s_rad *rad, float *scale, struct s_shift *shift)
+static t_coord		fdf_rot(t_pixel *elem, struct s_rad *rad,
+	float *scale, struct s_shift *shift)
 {
 	t_coord	coord;
 
-	coord.x = (int)round(((X * cos(RAD_Y) + (-Y * sin(RAD_X) + Z * cos(RAD_X)) * sin(RAD_Y))
-				* cos(RAD_Z) - (Y * cos(RAD_X) + Z * sin(RAD_X)) * sin(RAD_Z)) * *scale) + SHIFT_X;
-	coord.y = (int)round((-(X * cos(RAD_Y) + (-Y * sin(RAD_X) + Z * cos(RAD_X)) * sin(RAD_Y))
-				* sin(RAD_Z) + (Y * cos(RAD_X) + Z * sin(RAD_X)) * cos(RAD_Z)) * *scale) + SHIFT_Y;
+	coord.x = (int)round(((X * cos(RAD_Y) + (-Y * sin(RAD_X) + Z * cos(RAD_X))
+			* sin(RAD_Y)) * cos(RAD_Z) - (Y * cos(RAD_X) + Z * sin(RAD_X)) *
+				sin(RAD_Z)) * *scale) + SHIFT_X;
+	coord.y = (int)round((-(X * cos(RAD_Y) + (-Y * sin(RAD_X) + Z * cos(RAD_X))
+			* sin(RAD_Y)) * sin(RAD_Z) + (Y * cos(RAD_X) + Z * sin(RAD_X)) *
+				cos(RAD_Z)) * *scale) + SHIFT_Y;
 	return (coord);
 }
 
-void				fdf_map_to_img(t_map *map, int *img)
+static void			parse_line(size_t i, int *img, t_map *map)
 {
 	t_coord	curr;
 	t_coord	next;
 	t_coord	up;
-	size_t	i;
 	size_t	j;
+
+	j = 0;
+	while (j < MAX_X)
+	{
+		curr = fdf_rot(&MAP[i * MAX_X + j], &map->rad,
+						&map->scale, &map->shift);
+		if (j + 1 != MAX_X)
+		{
+			next = fdf_rot(&MAP[i * MAX_X + j + 1], &map->rad,
+							&map->scale, &map->shift);
+			fdf_line_create(img, curr, &next);
+		}
+		if (i)
+		{
+			up = fdf_rot(&MAP[(i - 1) * MAX_X + j], &map->rad,
+							&map->scale, &map->shift);
+			fdf_line_create(img, curr, &up);
+		}
+		++j;
+	}
+}
+
+void				fdf_map_to_img(t_map *map, int *img)
+{
+	size_t	i;
 
 	i = 0;
 	while (i < MAX_Y)
 	{
-		j = 0;
-		while (j < MAX_X)
-		{
-			curr = fdf_rot(&MAP[i * MAX_X + j], &map->rad, &map->scale, &map->shift);
-			if (j + 1 != MAX_X)
-			{
-				next = fdf_rot(&MAP[i * MAX_X + j + 1], &map->rad, &map->scale, &map->shift);
-				fdf_line_create(img, curr, &next);
-			}
-			if (i)
-			{
-				up = fdf_rot(&MAP[(i - 1) * MAX_X + j], &map->rad, &map->scale, &map->shift);
-				fdf_line_create(img, curr, &up);
-			}
-			++j;
-		}
+		parse_line(i, img, map);
 		++i;
 	}
 }
 
-void			fdf_work_frame(t_mlx *mlx)
+void				fdf_work_frame(t_mlx *mlx)
 {
 	mlx_clear_window(MLX_PTR, MLX_WIN);
 	ft_bzero(mlx->img, WIN_X * WIN_Y * 4);
